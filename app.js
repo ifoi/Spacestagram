1007f0aa815ea0e2229d2216172c66de562fd87f
Ifo Ikede <ifoi@users.noreply.github.com>
2022-01-16 01:18:18 -0400

    update so images are refreshed on click

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ function creatCard(images) {
     
     const container = document.getElementById("container") ;
         //  container.classList.add("row row-cols-auto") ;
+        
+        //clear container
+        container.innerHTML= "" ;
 
     images.forEach( (image , index ) =>{
         
@@ -109,9 +112,9 @@ function addLikes() {
 }
 
 function addShareBtns( imageURL){
-  let fbHtml = ` <hr> <strong> Sharing is caring :) </strong> <p>  <a href= "https://www.facebook.com/sharer.php?u=${imageURL}"  class="fa fa-facebook" target="blank" alt="share on facebook"></a> `
+  let fbHtml = ` <hr> <a href= "https://www.facebook.com/sharer.php?u=${imageURL}"  class="fa fa-facebook" target="blank" alt="share on facebook"></a> `
   let twitterHtml = `<a href="https://twitter.com/share?url=${imageURL}" class="fa fa-twitter" target="blank" alt="share on twitter"></a>`  ;
-  let linkedinHtml = `<a href="https://www.linkedin.com/shareArticle?url=${imageURL}" class="fa fa-linkedin" target="blank" alt="share on linkedIn"></a> </p>` ;
+  let linkedinHtml = `<a href="https://www.linkedin.com/shareArticle?url=${imageURL}" class="fa fa-linkedin" target="blank" alt="share on linkedIn"></a> ` ;
    
   shareHtml = fbHtml + twitterHtml + linkedinHtml
   
